33b1e3873fe695bfaa5434cada9498e8700d7818
hyunkyun31 <piehyun31@gmail.com>
2026-07-24 18:37:38 +0900

    Add JWT auth endpoints and protected /api/me/

--- api/views.py
+++ api/views.py
@@ -1,6 +1,17 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 @api_view(['GET'])
 def health_check(request) :
     return Response({"status" : "ok", "message" : "ANGIO CDSS 백엔드 서버가 정상 작동 중입니다."})
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def me(request) :
+    user = request.user
+    return Response({
+        "id" : user.id,
+        "username" : user.username,
+        "email" : user.email,
+    })
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import health_check
-
+from api.views import health_check, me
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/health/", health_check), # 테스트용 API 엔드포인트
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/me/", me),
 ]
